b6388b105b6adc147951b001692e8226b603d271
tedmaranets <TMaranets@RMDInc.com>
2019-10-15 16:18:06 -0400

    10/15/19 - 1

---
+++ modstructure/venv/Scripts/version_1.2/mod_materials_12.py
@@ -0,0 +1,39 @@
+import numpy
+import pandas
+
+# data list array
+materials = [["AlGaAs (30%Al 70%Ga)", 12.048, 40.5, "D19.1 Wafer", "AlGaAs_D191.csv"],
+             ["4H-SiC (UVA)", 9.7, 144, "High Detection \n Sensitivity of \n Ultraviolet \n 4H-SiC APDs \n- UVA", "4HSiC_UVA.csv"],
+             ["4H-SiC (China)", 9.7, 144, "4H-SiC APD \n Linear Array", "4HSiC_China.csv"]]
+
+class Material(object):
+
+    def __init__(self, name, er, stop, design, data_path):
+        self.name = name
+        self.er = er
+        self.stop = stop
+        self.design = design
+        self.data_path = data_path # in data folder, as .csv file
+
+
+def make_pnd(name): # creates an object with necessary properties for calculations
+    for i in range(len(materials)):
+        if name == materials[i][0]:
+            pnd = Material(name, materials[i][1], materials[i][2], materials[i][3], materials[i][4])
+
+    return pnd
+
+def make_layer_array(pnd):
+    layer_data = pandas.read_csv(pnd.data_path, header=None, sep=',')
+    layer_array = numpy.array(layer_data)
+    return layer_array
+
+def get_choices():
+    names = []
+    designs = []
+    for i in range(len(materials)):
+        names.append(materials[i][0])
+        designs.append(materials[i][3])
+    info_arrs = numpy.stack((names,designs),axis=0)
+
+    return info_arrs
